3b4c7119a73d5df5646f9ea984c088633c1c1439
jones-trae <jones.trae@gmail.com>
2021-09-14 09:46:43 -0700

    main and file outlining

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import React from 'React';
+import Header from '/Header.js';
+import Main from 'Main.js';
+import Footer from '/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header><h1>Horned Beast</h1>
-      </header>
+    // // <div className="App">
+    //   <header><h1>Horned Beast</h1>
+    //   </header>
+    // </div>
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+
     </div>
   );
 }
